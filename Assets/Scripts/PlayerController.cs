be5860f5e5d2b119b0963ad37cb6b4baf581a4fa
Austin Bryant <austindbryant@gmail.com>
2020-02-06 02:05:15 -0800

    shotgun spray

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : BasePlayer {
     public float speed;
     public double angle;
     public Sprite frontLeft, frontRight, frontDown, back;
+    public ParticleSystem shotgunBlast;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb2d;
 
@@ -19,23 +20,33 @@ public class PlayerController : BasePlayer {
     void Update() {
         Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
-        Vector2 diff = (position - mouse);
+        Vector2 diff = (mouse - position);
         angle = Math.Atan(diff.y / diff.x) * 180/Math.PI;
 
-        if((angle >= -45 && angle < 45) && diff.x < 0) {
+        if((angle >= -45 && angle < 45) && diff.x > 0) {
             spriteRenderer.sprite = frontRight;
-        } else if((angle >= -45 && angle < 45) && diff.x > 0) {
+            shotgunBlast.transform.rotation = new Quaternion(90,0,90,0);
+            shotgunBlast.transform.position = new Vector2(transform.position.x + 1.25f, transform.position.y - 0.8f);
+        } else if((angle >= -45 && angle < 45) && diff.x < 0) {
             spriteRenderer.sprite = frontLeft;
-
-        } else if((angle >= 45 || angle < -45) && diff.y > 0) {
-            spriteRenderer.sprite = frontDown;
+            shotgunBlast.transform.rotation = new Quaternion(90,0,-90,0);
+            shotgunBlast.transform.position = new Vector2(transform.position.x - 1.25f, transform.position.y - 0.8f);
         } else if((angle >= 45 || angle < -45) && diff.y < 0) {
+            spriteRenderer.sprite = frontDown;
+            shotgunBlast.transform.rotation = new Quaternion(0,-90,90,0);
+            shotgunBlast.transform.position = new Vector2(transform.position.x - 0.3f, transform.position.y - 0.75f);
+        } else if((angle >= 45 || angle < -45) && diff.y > 0) {
             spriteRenderer.sprite = back;
+            shotgunBlast.transform.rotation = new Quaternion(0,-90,-90,0);
         }
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         transform.Translate(new Vector2(moveHorizontal, moveVertical) * speed * Time.deltaTime);
 
+        if(Input.GetMouseButtonDown(0)) {
+            Debug.Log("Boom");
+            shotgunBlast.Play();
+        }
     }
 }
