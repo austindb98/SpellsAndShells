5d1b5c0b05366a070924e57ca1227a05362c777d
Kyle Chin <kylechin01@gmail.com>
2020-02-06 14:47:44 -0800

    Added raycasted bullets, collidable tiles, and breakable pots. Also added a rudimentary sound system - delete it after demo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using System;
 
 public class PlayerController : BasePlayer {
@@ -12,15 +13,24 @@ public class PlayerController : BasePlayer {
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb2d;
 
+    public GameObject soundManager;
+    public LayerMask wallLayer, obstacleLayer;
+    private LayerMask interactsWithBullets;
+    private int numberPellets = 10;
+    private float pelletAngleVariance = 0.05f; // in radians
+    private float mouseAngle;
+    private Vector2 mousePos;
+
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        interactsWithBullets = wallLayer | obstacleLayer;
     }
 
     void Update() {
-        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
-        Vector2 diff = (mouse - position);
+        Vector2 diff = (mousePos - position);
         angle = Math.Atan(diff.y / diff.x) * 180/Math.PI;
 
         if((angle >= -45 && angle < 45) && diff.x > 0) {
@@ -47,6 +57,55 @@ public class PlayerController : BasePlayer {
         if(Input.GetMouseButtonDown(0)) {
             Debug.Log("Boom");
             shotgunBlast.Play();
+            shoot();
         }
+        if (Input.GetMouseButton(1))
+        {
+            shoot();
+        }
+    }
+
+    private float getMouseAngle()
+    {
+        return Mathf.Atan2(this.mousePos.y - this.transform.position.y,
+            this.mousePos.x - this.transform.position.x);
+    }
+
+    private void shoot()
+    {
+        soundManager.GetComponent<SoundController>().playShotgunShootSound();
+        mouseAngle = getMouseAngle();
+        Vector2[] pelletDirections = new Vector2[10];
+        for(int i = 0; i < pelletDirections.Length; ++i)
+        {
+            float appliedAngle = mouseAngle + pelletAngleVariance * (i / 2);
+            if (i % 2 == 0)
+            {
+                appliedAngle = mouseAngle - pelletAngleVariance * (i / 2);
+            }
+            
+            pelletDirections[i] = new Vector2(Mathf.Cos(appliedAngle), Mathf.Sin(appliedAngle));
+            
+            RaycastHit2D raycastResult = 
+                Physics2D.Raycast(this.transform.position, pelletDirections[i], Mathf.Infinity, interactsWithBullets);
+            if(raycastResult.collider != null)
+            {
+                Debug.DrawLine(this.transform.position, raycastResult.point, Color.red, 0.1f);
+                if(raycastResult.collider.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
+                {
+                    GameObject tileMapGameObject = raycastResult.collider.gameObject;
+                    Tilemap map = tileMapGameObject.GetComponent<Tilemap>();
+                    var tilePos = tileMapGameObject.GetComponent<GridLayout>().WorldToCell(raycastResult.point);
+                    map.SetTile(tilePos, null);
+                    soundManager.GetComponent<SoundController>().playPotBreakSound();
+                }
+            }
+            else
+            {
+                Debug.DrawRay(this.transform.position, pelletDirections[i] * 100, Color.yellow, 0.1f);
+            }
+            
+        }
+        
     }
 }
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioClip shotgunShootSound;
+    public AudioClip potBreakSound;
+
+    private AudioSource soundSource;
+
+    void Start()
+    {
+        soundSource = GetComponent<AudioSource>();
+    }
+
+    public void playShotgunShootSound()
+    {
+        soundSource.PlayOneShot(shotgunShootSound, 1.0f);
+    }
+
+    public void playPotBreakSound()
+    {
+        soundSource.PlayOneShot(potBreakSound, 1.0f);
+    }
+}
