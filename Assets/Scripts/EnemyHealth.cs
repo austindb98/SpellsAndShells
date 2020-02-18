6a3791ff6fe5de42d2e70800b59959c7fbac8eda
jjohn163 <jjohn163@calpoly.edu>
2020-02-18 13:28:35 -0800

    added text damage particles to bullet collisions

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float maxHealth;
     private float currentHealth;
     public EnemyController enemyController;
+    public ParticleSystem damageText;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        damageText = Resources.Load<ParticleSystem>("ParticleSystems/TextParticles");
     }
 
     // Update is called once per frame
@@ -22,7 +25,12 @@ public class EnemyHealth : MonoBehaviour
 
     public void takeDamage(float damage) {
         currentHealth -= damage;
+        ParticleSystem text =  Instantiate(damageText);
+        text.transform.position = transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+        text.GetComponentInChildren<UnityEngine.UI.Text>().text = damage.ToString();
 
+        
+        text.Play();
         if(currentHealth <= 0f)
             enemyController.handleEnemyDeath();
         else
