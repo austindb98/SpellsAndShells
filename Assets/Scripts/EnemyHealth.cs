1d9272d9cfa084c1e383d4dc8b78c94fc2250e98
jjohn163 <jjohn163@calpoly.edu>
2020-02-18 13:57:51 -0800

    combined damage taken over a few frames to avoid collection of damage particles

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -9,28 +9,40 @@ public class EnemyHealth : MonoBehaviour
     private float currentHealth;
     public EnemyController enemyController;
     public ParticleSystem damageText;
+    private float damageUpdateTime;
+    private static float deltaDamageTime;
+    private float accumulatedDamage;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         damageText = Resources.Load<ParticleSystem>("ParticleSystems/TextParticles");
+        damageUpdateTime = Time.time + deltaDamageTime;
+        accumulatedDamage = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(damageUpdateTime < Time.time)
+        {
+            damageUpdateTime = Time.time + deltaDamageTime;
+            if(accumulatedDamage > 0)
+            {
+                ParticleSystem text = Instantiate(damageText);
+                text.transform.position = transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+                text.GetComponentInChildren<UnityEngine.UI.Text>().text = accumulatedDamage.ToString();
+                text.Play();
+            }
+            accumulatedDamage = 0;
+        }
         
     }
 
     public void takeDamage(float damage) {
         currentHealth -= damage;
-        ParticleSystem text =  Instantiate(damageText);
-        text.transform.position = transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
-        text.GetComponentInChildren<UnityEngine.UI.Text>().text = damage.ToString();
-
-        
-        text.Play();
+        accumulatedDamage += damage;
         if(currentHealth <= 0f)
             enemyController.handleEnemyDeath();
         else
