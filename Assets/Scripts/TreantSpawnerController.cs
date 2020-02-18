1847e8c1dfcfc47cd3ac0686050e185cd6093a97
Linn <LinnRising2012@gmail.com>
2020-02-18 12:24:05 -0800

    knockback more reasonable and can kill spawners now

---
+++ Assets/Scripts/TreantSpawnerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreantSpawnerController : EnemyController
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        
+    }
+
+    public override void handleKnockback(float knockbackMagnitude) {}
+
+    public override void handleEnemyDeath() {
+        Destroy(gameObject);
+    }
+}
