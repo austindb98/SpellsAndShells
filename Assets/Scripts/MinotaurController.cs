bb6e0d13f306e899cfe689ec36cfc788ac2562e7
Nick <ntteng@calpoly.edu>
2020-02-19 14:18:27 -0800

    added death animation

--- Assets/Scripts/MinotaurController.cs
+++ Assets/Scripts/MinotaurController.cs
@@ -17,6 +17,10 @@ public class MinotaurController : EnemyController
     private float swingRestTime = 1f;
     private float swingRestTimer = 0f;
 
+    private float deathTimer = 0f;
+    private float deathTime = 1.2f;
+    private bool isDead = false;
+
     public GameObject player;
     private Animator an;
     public AIPath aiPath;
@@ -53,7 +57,7 @@ public class MinotaurController : EnemyController
                     knockbackTimer = 0f;
                 }
             }
-            if(!isKnockback && !isSwingRest) {
+            if(!isKnockback && !isSwingRest && !isDead) {
                 aiPath.canMove = true;
             }
         }
@@ -70,6 +74,15 @@ public class MinotaurController : EnemyController
             an.SetBool("isWalking", true);
         }
         
+
+        if(isDead)
+        {
+            deathTimer += Time.deltaTime;
+            if(deathTimer > deathTime)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -82,7 +95,8 @@ public class MinotaurController : EnemyController
     public override void handleShotgunHit(float knockbackMagnitude) {
         Vector2 unitVec = transform.position - player.transform.position;
         unitVec.Normalize();
-        rb2d.AddForce(unitVec * knockbackMagnitude * 0.5f);
+        if(!isDead)
+            rb2d.AddForce(unitVec * knockbackMagnitude * 0.5f);
         isKnockback = true;
         aiPath.canMove = false;
         an.SetBool("isWalking", false);
@@ -90,7 +104,10 @@ public class MinotaurController : EnemyController
     }
 
     public override void handleEnemyDeath() {
-        Destroy(gameObject);
+        aiPath.canMove = false;
+        an.SetBool("isDead", true);
+        isDead = true;
+        //Destroy(gameObject);
     }
 
     private void WalkLeft()
