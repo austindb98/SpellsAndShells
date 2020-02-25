063d3948378a554e4ffa56ad6e4ef59c2cd76f39
jjohn163 <jjohn163@calpoly.edu>
2020-02-24 20:01:11 -0800

    added key doors functionality

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponentInParent<PlayerController>();
+        if (player)
+        {
+            GateController gate = transform.parent.GetComponentInChildren<GateController>();
+            if (gate)
+            {
+                gate.keyFound = true;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
