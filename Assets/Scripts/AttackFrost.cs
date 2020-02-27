70a55456d9138bbb68bd05977a5b0397ea9f2c9d
Linn <LinnRising2012@gmail.com>
2020-02-27 09:37:17 -0800

    added slowing functionality for frost spells

--- Assets/Scripts/AttackFrost.cs
+++ Assets/Scripts/AttackFrost.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class AttackFrost : BaseAttack
 {
     private float frostDmg = 50f;
+
+    private float slowMag = 0.6f;
+    private float slowDur = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
         speed = 8;
         damage = 20;
         element = Element.Ice;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collider) {
+        EnemyController enemyController = collider.gameObject.GetComponent<EnemyController>();
+        enemyController.applyFrostSlowingEffect(slowMag, slowDur);
+        base.OnTriggerEnter2D(collider);
     }
 
 
--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -18,37 +18,44 @@ public class EnemyController : MonoBehaviour
     public bool isKnockback = false;
     private float knockbackTimer = 0f;
     private float knockbackTime = 0.8f;
+    
+    private bool isFrozen;
+    private float frozenTimer;
+    private float frozenTime;
 
     public PlayerController playerController;
 
     public float knockbackCoefficient;
+
+    public float maxSpeed;
 
     public GameObject player;
     public AIPath aiPath;
 
     public SpawnMaster spawnMaster;
 
+    public SpriteRenderer spriteRenderer;
+
     virtual public void Start() {
         if(!player)
-                player = GameObject.FindWithTag("Player");
+            player = GameObject.FindWithTag("Player");
 
         an = gameObject.GetComponent<Animator>();
         playerCollider = player.GetComponent<Collider2D>();
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         playerController = player.GetComponent<PlayerController>();
+        maxSpeed = aiPath.maxSpeed;
 
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         gameObject.GetComponent<AIDestinationSetter>().target = player.transform;
     }
 
     virtual public void Update() {
-        if(isKnockback) {
-            knockbackTimer += Time.deltaTime;
-            if(knockbackTimer > knockbackTime) {
-                isKnockback = false;
-                aiPath.canMove = true;
-                knockbackTimer = 0f;
-            }
-        }
+        if(isFrozen)
+            handleFrozen();
+
+        if(isKnockback)
+            handleKnockback();
 
     }
 
@@ -65,5 +72,31 @@ public class EnemyController : MonoBehaviour
         }
         if(spawnMaster)
             spawnMaster.removeEnemyFromList(this);
+    }
+
+    public void applyFrostSlowingEffect(float magnitude, float time) {
+        aiPath.maxSpeed *= magnitude;
+        isFrozen = true;
+        frozenTime = time;
+        frozenTimer = 0f;
+        spriteRenderer.color=new Color(0.5f, 0.5f, 1, 1);
+    }
+
+    private void handleFrozen() {
+        frozenTimer += Time.deltaTime;
+        if(frozenTimer > frozenTime) {
+            isFrozen = false;
+            aiPath.maxSpeed = maxSpeed;
+            spriteRenderer.color=new Color(1, 1, 1, 1);
+        }
+    }
+
+    private void handleKnockback() {
+        knockbackTimer += Time.deltaTime;
+        if(knockbackTimer > knockbackTime) {
+            isKnockback = false;
+            aiPath.canMove = true;
+            knockbackTimer = 0f;
+        }
     }
 }
