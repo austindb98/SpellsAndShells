984a6997c51869b27c54f0a591cf2b2993f7f2dc
jjohn163 <jjohn163@calpoly.edu>
2020-02-26 17:22:54 -0800

    added a dialog manager

---
+++ Assets/Scripts/DialogScheduler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class Dialog
+{
+    public string text;
+    public int activeTime;
+    public float closeTime;
+    public Dialog(string text, int activeTime)
+    {
+        this.text = text;
+        this.activeTime = activeTime;
+    }
+}
+
+public class DialogScheduler : MonoBehaviour
+{
+    private Transform dialogBox;
+    private Text dialogText;
+    private Queue<Dialog> dialogs;
+    private Dialog currentDialog;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogs = new Queue<Dialog>();
+        dialogBox = transform.Find("DialogBox");
+        dialogText = dialogBox.GetComponentInChildren<Text>();
+        dialogBox.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogBox.gameObject.activeSelf)
+        {
+            if (Time.time > currentDialog.closeTime && currentDialog.activeTime > 0)
+            {
+                dialogBox.gameObject.SetActive(false);
+                updateDialog();
+            }
+        }
+        else
+        {
+            updateDialog();
+        }
+    }
+
+    void updateDialog()
+    {
+        if (dialogs.Count > 0)
+        {
+            dialogBox.gameObject.SetActive(true);
+            currentDialog = dialogs.Dequeue();
+            currentDialog.closeTime = Time.time + currentDialog.activeTime;
+            dialogText.text = currentDialog.text;
+        }
+    }
+
+    public void addDialog(string text, int activeTime)
+    {
+        dialogs.Enqueue(new Dialog(text, activeTime));
+    }
+
+    public void closeCurrentDialog()
+    {
+        dialogBox.gameObject.SetActive(false);
+    }
+}
