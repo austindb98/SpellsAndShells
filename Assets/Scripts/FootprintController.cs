8e2bbaf9ffe74533e9d46014c59001d566b522f5
jjohn163 <jjohn163@calpoly.edu>
2020-03-02 18:22:16 -0800

    added footsteps to the ground where player stepped

---
+++ Assets/Scripts/FootprintController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootprintController : MonoBehaviour
+{
+    private float creationTime;
+    private static float lifeTime = 1;
+    private SpriteRenderer renderer;
+    private static float baseTransparency = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        creationTime = Time.time;
+        renderer = transform.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > creationTime + lifeTime)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Color color = renderer.color;
+            float transparency = ((lifeTime - (Time.time - creationTime)) / lifeTime) * baseTransparency;
+            color.a = transparency;
+            renderer.color = color;
+
+        }
+    }
+}
---
+++ Assets/Scripts/FootprintSpawner.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FootprintSpawner : MonoBehaviour
+{
+    private Vector3 lastPosition;
+    private float lastTime;
+    private static float deltaTime = 1f;
+    public GameObject footprintPrefab;
+    private Vector2 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateFootprint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, lastPosition) > 1 || (Time.time - lastTime) > 1)
+        {
+            CreateFootprint();
+        }
+    }
+    private float getMouseAngle()
+    {
+        return Mathf.Atan2(mousePos.y - transform.position.y,
+            mousePos.x - transform.position.x);
+    }
+    
+    private float getFootprintAngle()
+    {
+        float angle = (float)(getMouseAngle() / Math.PI * 180);
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        Vector2 diff = (mousePos - position);
+
+        if ((angle >= -22.5 && angle < 22.5) && diff.x > 0)
+        {
+            //frontRight;
+            angle = -90;
+        }
+        else if ((angle >= 22.5 && angle < 67.5) && diff.x > 0)
+        {
+            //diagUpRight;
+            angle = -45;
+        }
+        else if ((angle >= 67.5 && angle < 112.5) && diff.y > 0)
+        {
+            //spriteRenderer.sprite = back;
+            angle = 0;
+
+        }
+        else if ((angle >= 112.5 && angle < 157.5) && diff.y > 0)
+        {
+            //diagUpLeft;
+            angle = 45;
+
+        }
+        else if ((angle >= 157.5 || angle < -157.5) && diff.x < 0)
+        {
+            //frontLeft;
+            angle = 90;
+
+        }
+        else if ((angle >= -157.5 && angle < -112.5) && diff.x < 0)
+        {
+            //diagDownLeft;
+            angle = 135;
+
+        }
+        else if ((angle >= -112.5 && angle < -67.5) && diff.y < 0)
+        {
+            //frontDown;
+            angle = 180;
+        }
+        else if ((angle >= -67.5 && angle < -22.5) && diff.x > 0)
+        {
+            //diagDownRight;
+            angle = -135;
+        }
+
+        return angle;
+    }
+    
+
+    void CreateFootprint()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lastTime = Time.time;
+        lastPosition = transform.position;
+        GameObject footprint = Instantiate(footprintPrefab);
+        Vector3 placement = transform.position;
+        placement.y = placement.y - 0.7f;
+        Quaternion rot = Quaternion.Euler(0, 0, getFootprintAngle());
+        footprint.transform.SetPositionAndRotation(placement, rot);
+    }
+}
