fc18ce5be83c806941f45555aa6ca4d60ca61c7e
Linn <LinnRising2012@gmail.com>
2020-03-12 14:00:31 -0700

    fire totem ready to rumble

---
+++ Assets/Scripts/Enemies/FireTotemAttackController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTotemAttackController : EnemyController
+{
+    private bool isSeeking = false;
+    private float speed = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(10f, 10f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isSeeking) {
+            Vector3 towardsPlayer = player.transform.position - transform.position;
+            towardsPlayer.Normalize();
+
+            rb2d.velocity = speed * (new Vector2(towardsPlayer.x, towardsPlayer.y));
+        }
+    }
+
+    public override void handleShotgunAttack(int damage) {
+        isSeeking = false;
+        rb2d.velocity = new Vector2(0f, 0f);
+        an.SetBool("isExplode", true);
+    }
+
+    public override void handleAttack(float damage, BaseAttack.Element element) {
+        isSeeking = false;
+        rb2d.velocity = new Vector2(0f, 0f);
+        an.SetBool("isExplode", true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.gameObject.tag != "Enemy") {
+            isSeeking = false;
+            rb2d.velocity = new Vector2(0f, 0f);
+            an.SetBool("isExplode", true);
+            if(collider == playerCollider)
+                playerController.takeDamage(10f);
+        }
+    }
+
+    public void handleExplosionFinish() {
+        Destroy(gameObject);
+    }
+
+    public void handleLaunchExplosion() {
+        Vector2 velocity = new Vector2(player.transform.position.x - transform.position.x,
+                                       player.transform.position.y - transform.position.y);
+        velocity.Normalize();
+        rb2d.velocity = speed * velocity;
+        an.SetBool("isLaunched", true);
+        transform.localScale = new Vector3(1.5f, 1.5f, 1f);
+        isSeeking = true;
+        GetComponent<Collider2D>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/Enemies/FireTotemController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTotemController : EnemyController
+{
+    private float cooldownTimer = 0f;
+    private float cooldownTime = 2f;
+    private bool isCooldown;
+
+    private float maxTotemRange = 20f;
+
+    private int raycastLayerMask;
+
+    public GameObject explosiveAttackPrefab;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+        raycastLayerMask =  ((1 << LayerMask.NameToLayer("Obstacles")) |
+                        (1 << LayerMask.NameToLayer("Walls")) |
+                        (1 << LayerMask.NameToLayer("Player")));
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        if(player.transform.position.x < transform.position.x)
+            transform.localScale = new Vector3(-8, 8, 1);
+        else
+            transform.localScale = new Vector3(8, 8, 1);
+
+        if(isCooldown) {
+            cooldownTimer += Time.deltaTime;
+            if(cooldownTimer > cooldownTime) {
+                isCooldown = false;
+                cooldownTimer = 0f;
+            }
+        }
+        else if(CheckLineOfSight()) {
+            launchExplosiveAttack();
+        }
+        
+    }
+
+    public void handleAttack() {
+        Vector3 pos = transform.position + new Vector3(0f, 0.3f, 0);
+        Quaternion q = Quaternion.Euler( 0f, 0f, 0f );
+
+        GameObject thisAttack = Instantiate(explosiveAttackPrefab, pos, q);
+        thisAttack.GetComponent<EnemyController>().player = player;
+
+        an.SetBool("isAttack", false);
+    }
+
+    public override void handleEnemyDeath() {
+        base.handleEnemyDeath();
+        base.spawnMaster.isRescan = true;
+        Destroy(gameObject);
+    }
+
+    public void launchExplosiveAttack() {
+        an.SetBool("isAttack", true);
+        isCooldown = true;
+    }
+
+    // returns whether the player is in LoS of the ArcherBoy
+    private bool CheckLineOfSight() {
+        bool isAllHit = true;
+
+        if(Vector3.Distance(player.transform.position, transform.position) < maxTotemRange) {
+            Vector3[] rayStartingPoints = {
+                transform.position + new Vector3(0.5f, 0, 0),
+                transform.position + new Vector3(-0.5f, 0, 0),
+                transform.position + new Vector3(0, 0.5f, 0),
+                transform.position + new Vector3(0, -0.5f, 0)
+            };
+            foreach(Vector3 initialPos in rayStartingPoints) {
+                Vector3 rayDirection = player.transform.position - initialPos;
+                RaycastHit2D hit = Physics2D.Raycast(initialPos, rayDirection, maxTotemRange, raycastLayerMask);
+                if(!hit || hit.transform != player.transform)
+                    isAllHit = false;
+            }
+
+            return isAllHit;
+        }
+        return false;
+    }
+}
