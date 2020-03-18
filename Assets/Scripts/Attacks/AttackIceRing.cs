4eb5199a401b878c66bd7f3c2181f87e8328a212
Linn <LinnRising2012@gmail.com>
2020-03-18 15:02:27 -0700

    fixed null reference from icering

--- Assets/Scripts/Attacks/AttackIceRing.cs
+++ Assets/Scripts/Attacks/AttackIceRing.cs
@@ -10,7 +10,7 @@ public class AttackIceRing : ChildAttack
     private void OnTriggerEnter2D(Collider2D collision)
     {
         EnemyController enemyController = collision.gameObject.GetComponent<EnemyController>();
-        if (enemyController && enemyController.gameObject.tag != "DestructibleSpell") {
+        if (enemyController && enemyController.aiPath != null && enemyController.gameObject.tag != "DestructibleSpell") {
             enemyController.applyFrostSlowingEffect(AttackFrost.slowMag, AttackFrost.slowDur);
         }
     }
