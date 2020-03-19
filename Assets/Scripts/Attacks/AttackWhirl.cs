a2ce331bd20b97f959b94a8b264d2043ebaddef0
Michael Curtis <3968713+mcprog@users.noreply.github.com>
2020-03-18 18:58:35 -0700

    added wind lvl 2 spell!

---
+++ Assets/Scripts/Attacks/AttackWhirl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackWhirl : AttackGust
+{
+    /*protected override void Update()
+    {
+        if (canMove)
+        {
+            //Vector3 movement = 
+            transform.Translate(transform.localRotation * Vector3.down * speed * Time.deltaTime, gameObject.transform.parent);
+        }
+    }*///im stupid
+}
